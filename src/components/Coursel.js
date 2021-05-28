4262c44a9c69fca28a5304fea6c0dd5de6bf4960
praveen sokhal <kaurswt21@gmail.com>
2021-05-28 10:09:41 +0530

    fresh code

---
+++ src/components/Coursel.js
@@ -0,0 +1,19 @@
+let Coursel = () =>{
+    return (
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="%PUBLIC_URL%/background1.jpg" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="%PUBLIC_URL%/background1.jpg"  className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="%PUBLIC_URL%/background1.jpg"  className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+</div>
+    )
+}
+
+export default Coursel;
